c7ddb1a23e14adb487918fda662776af8e7f16b7
Aaditya Shah <aadishah1020@gmail.com>
2025-06-09 18:23:17 +0545

    Stop tracking .env and ignore it going forward

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const PORT = 8080;
+const app = express();
+const path = require('path');
+const ejs = require('ejs');
+const ejsMate = require('ejs-mate');    
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+
+const sessionConfig = {
+    secret: 
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate); 
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+
